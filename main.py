3d6c4ae12d34bec1e60b1ded5e9c8fde6b49e1f1
Shashank-max <65381727+Shashank-max@users.noreply.github.com>
2023-01-18 20:49:52 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+a = int(input("Enter the Value of a = "))
+b = int(input("Enter the Value of b = "))
+c = int(input("Enter the Value of c = "))
+add = a+b
+print("Addition of", a, "and", b, "is", add)
+sub = a-b
+print("Substraction of",a, "and", b, "is", sub)
+mul = a*b
+print("Multiplication of", a, "of", b, "is", mul)
+div = a/b
+print("Division of", a, "and", b, "is", div)
+div2 = a/c
+print("Division of", a, "and", c, "is", div2)
+exp = a**b
+print("Exponential of", a, "to the power", b, "is", exp)
+flordiv = a//c
+print("Floor Division of", a, "and", c, "is", flordiv)
+mod = a%c
+print("Modulus of", a, "and", c, "is", mod)
+
